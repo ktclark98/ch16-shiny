a1a54326f5c39af21dbe510223cf2f0221e6f38c
Katie Clark <98ktclark@gmail.com>
2017-11-14 13:49:42 -0800

    Add plot function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,12 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+BuildScatterPlot <- function(this.x, this.y, this.color, this.title = "Title", x.title = "X Title", y.title = "Y Title") {
+  p <- plotly(x = this.x, y = this.y, color = this.color, name = title,
+         type = "scatter") %>% 
+    layout(title = this.title,
+           xaxis = x.title,
+           yaxis = y.title)
+  return(p)
+}
